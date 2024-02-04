5ac506fe628f9bb488cc769a1f7b5f965bde51fa
rifqiazizm <rifqiazizsmg@gmail.com>
2024-02-04 14:56:26 +0700

    final streamlits

--- coba.py
+++ coba.py
@@ -484,7 +484,9 @@ class Strimlit(object):
         st.text("")
         st.text("")   
 
-        if df:
+
+
+        if len(self.df_encoded) > 0 :
             qtySales = df[['area','id_produk']].groupby('id_produk').count().reset_index().copy()
             lowSales =  qtySales[qtySales['area'] == 1]['id_produk'].tolist()
             highSales = qtySales.sort_values(by='area',ascending=False)['id_produk'].head(50).tolist()
